3ab831b8c14d0ab3283f749130409d482c819b92
supersonic <guptasanchit16@gmail.com>
2019-09-17 05:37:40 +0530

    Update settings.py

--- dapjak/settings.py
+++ dapjak/settings.py
@@ -129,8 +129,6 @@ USE_L10N = True
 
 USE_TZ = True
 
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 STATIC_URL = '/static/'
